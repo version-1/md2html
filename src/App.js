b27b7e2cdb1eb080f0e2298b4d41e53e8c265780
version1 <12.05.xxxx@gmail.com>
2018-10-19 23:39:50 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {makeHtml} from './showdown';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: '',
+      html: '',
+    };
+
+    this.updateHTML = event => this.setState({html: event.target.value});
+    this.updateMarkdown = this.updateMarkdown.bind(this);
+  }
+
+  updateMarkdown(event) {
+    const {value} = event.target;
+    this.setState({
+      markdown: value,
+      html: makeHtml(value),
+    });
+  }
+
   render() {
+    const {markdown, html} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="header">
+          <h1>MD2HTML</h1>
         </header>
+        <div className="body">
+          <div className="markdown-container">
+            <div class="markdown-header">
+              <h2>Markdown</h2>
+            </div>
+            <div>
+              <textarea
+                rows="50"
+                value={markdown}
+                className="textarea markdown-area"
+                placeholder="write something great markdown!!"
+                onChange={this.updateMarkdown}
+                required
+              />
+            </div>
+          </div>
+          <div className="arrow-container">
+            <div className="arrow" />
+          </div>
+          <div className="html-container">
+            <div>
+              <h2>HTML</h2>
+            </div>
+            <div>
+              <textarea
+                rows="50"
+                value={html}
+                className="textarea html-area"
+                placeholder="output converted html with a genius algorithm"
+                onChange={this.updateHTML}
+                required
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
